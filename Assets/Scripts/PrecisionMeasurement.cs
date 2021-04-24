54485fe0fd64cd088a0a3e6955f69a645e5aeb03
KaiGarbeTema <garbe@tema.de>
2021-04-24 12:31:26 +0200

    2.3.b/c
    aufgaben wurden wie vorgegeben umgesetzt

--- Assets/Scripts/PrecisionMeasurement.cs
+++ Assets/Scripts/PrecisionMeasurement.cs
@@ -1,9 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit;
 
-public class PrecisionMeasurement : MonoBehaviour
+public class PrecisionMeasurement : MonoBehaviour, IMixedRealityPointerHandler
+
 {
+    //the transform of the center of the target
+    [SerializeField] private Transform targetCenterTransform;
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        // go over every input source
+        foreach (var source in CoreServices.InputSystem.DetectedInputSources)
+        {
+            // Ignore anything that is not a hand because we want articulated hands
+            if (source.SourceType == InputSourceType.Hand)
+            {
+                // find the pointers of the hand
+                foreach (IMixedRealityPointer p in source.Pointers)
+                {
+                    if (p is IMixedRealityNearPointer)
+                    {
+                        // Ignore near pointers, we only want the rays
+                        continue;
+                    }
+                    // if the pointer actually gives a result: do something with it
+                    if (p.Result != null)
+                    {
+                        // TODO: enter your code here
+                        // ===========================================
+                        Vector3 pointerEndPosition = p.Result.Details.Point;
+                        float distanceToTargetCenter = (pointerEndPosition - targetCenterTransform.position).magnitude;
+                        Debug.Log(distanceToTargetCenter);
+                        // ===========================================
+                    }
+
+                }
+            }
+        }
+    }
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
