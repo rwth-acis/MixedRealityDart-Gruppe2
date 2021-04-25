134960e4d82b824cfba5c9b4696645c4bfb05692
KaiGarbeTema <garbe@tema.de>
2021-04-25 15:14:44 +0200

    2.4.e
    console print every 5 seconds

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private float spawnInterval;
+    private float remainingTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        remainingTime = spawnInterval;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        remainingTime -= Time.deltaTime;
+        if (remainingTime <= 0) {
+            remainingTime = spawnInterval;
+            Debug.Log("Spawn Interval Passed");
+        }
     }
 }
